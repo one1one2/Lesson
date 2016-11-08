cc13c7e305b093294f442a507fbc0f331ee353ff
one1one2 <one1one2@126.com>
2016-11-08 13:48:57 +0800

    .

--- Parallel/homework2/cpi.c
+++ Parallel/homework2/cpi.c
@@ -6,22 +6,22 @@ double f( double a ){ return ( 4.0 /( 1.0 + a * a ) ); }
 
 int main(int argc, char *argv[])
 {
-	int n,myid,numprocs,i,namelen;
+	int n, myid, numprocs, i, namelen;
 	double PI25DT = 3.141592653589793238462643;
-	double mypi,pi,h,sum,x;
-	double startwtime,endwtime;
+	double mypi, pi, h, sum, x;
+	double startwtime, endwtime;
 	char processor_name[MPI_MAX_PROCESSOR_NAME];
 
-	MPI_Init(&argc,&argv);
-	MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
-	MPI_Comm_rank(MPI_COMM_WORLD,&myid);
-	MPI_Get_processor_name(processor_name,&namelen);
-	fprintf(stderr,"Process %d on %s\n",myid,processor_name);
-	if (myid ==0){
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+	MPI_Get_processor_name(processor_name, &namelen);
+	fprintf(stderr, "Process %d on %s\n", myid, processor_name);
+	if (myid == 0){
 		n = 10000;
 		startwtime = MPI_Wtime();
 	}
-	MPI_Bcast(&n , 1 , MPI_INT , 0 , MPI_COMM_WORLD);
+	MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	h = 1.0 / (double)n;
 	sum = 0.0;
 	for (i = myid; i < n; i+=numprocs){
@@ -30,12 +30,12 @@ int main(int argc, char *argv[])
 	}
 	mypi = h * sum;
 	MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-	if (myid ==0){
+	if (myid == 0){
 		endwtime = MPI_Wtime();
 		printf("pi is approximately %.16f, error is %.16f\n", pi, pi - PI25DT);
 		printf("wall clock time = %f\n", endwtime - startwtime);
 	}
-	printf("%d %d\n", namelen ,numprocs);
+	printf("%d %d\n", namelen, numprocs);
 	MPI_Finalize();
 
 	return 0;
